647674e635cdbb4f7eb05a7f6f377bc31a76ffe8
vidhya <vid.sigsoln@gmail.com>
2016-04-05 19:09:10 +0000

    show/hide markers based on search results

--- js/list.js
+++ js/list.js
@@ -7,7 +7,7 @@
 // https://maps.googleapis.com/maps/api/place/radarsearch/json?location=48.859294,2.347589&radius=5000&type=cafe&keyword=vegetarian&key=YOUR_API_KEY
 
 var bakeriesMarkers = [];
-var bookstoresMarkers = [];
+//var bookstoresMarkers = [];
 var parkingLotsMarkers = [];
 var geocoderSearchResult;
 var nyc = new google.maps.LatLng(40.7127, 74.0059);
@@ -56,13 +56,15 @@ function processFrsqrBooks(response) {
                             'rating': 0.0,
                             'lat': 0,
                             'lng': 0
-                        }
+                        },
+                        index: ''
                     }
                     frsqrItem.tips = items[i].tips;
                     frsqrItem.venue.name = items[i].venue.name;
                     frsqrItem.venue.venueUrl = items[i].venue.url;
                     frsqrItem.venue.lat = items[i].venue.location.lat;
                     frsqrItem.venue.lng = items[i].venue.location.lng;
+                    frsqrItem.venue.index = i;
                     if (items[i].venue.photos.count > 0) {
                         // there is at least one photo - so construct photo url. 
                         var groups = items[i].venue.photos.groups;
@@ -92,47 +94,6 @@ function processFrsqrBooks(response) {
     }
 }
 
-/*function processFrsqrBakeries() {
-    if (getBakeriesRequest.readyState === XMLHttpRequest.DONE) {
-        if (getBakeriesRequest.status === 200) {
-            var jsonResponse = JSON.parse(getBakeriesRequest.responseText);
-            var bak = [];
-            var frsqrBakeryItems = [];
-            if(jsonResponse.response.groups.length > 0){
-              bakVenues = jsonResponse.response.groups[0];
-              items = bakVenues.items;
-              for(var i=0; i < items.length; i++){
-                frsqrItem.tips = items[i].tips;
-                frsqrItem.venue.name = items[i].venue.name;
-                frsqrItem.venue.venueUrl = items[i].venue.url;
-                frsqrItem.venue.lat = items[i].venue.location.lat;
-                frsqrItem.venue.lng = items[i].venue.location.lng;
-
-                if(items[i].venue.photos.count > 0){
-                  // there is at least one photo - so construct photo url.
-                  var groups = items[i].venue.photos.groups;
-                  if(groups.length > 0){
-                    var photoItems = groups[0].items;
-                    if(photoItems.length > 0){
-                       frsqrItem.venue.venuePhotoUrl = photoItems[0].prefix + '50x50' + photoItems[0].suffix;
-                    }
-                  }
-                }
-
-                frsqrItem.venue.rating = items[i].venue.rating;
-                frsqrBakeryItems.push(frsqrItem);
-                bak[i]= {name: frsqrItem.venue.name, item: frsqrItem };
-              }
-              bakeryViewModel.bakeries(bak); 
-            }
-            bakeriesDetailsMarkers(map,'F',frsqrBakeryItems);
-        } else {
-            alert('There was a problem with the request.');
-        }
-    }
-}
-*/
-
 function bookstoresDetailsMarkers(map, label, frsqrBookItems) {
     var content = "";
     var marker;
@@ -152,7 +113,8 @@ function bookstoresDetailsMarkers(map, label, frsqrBookItems) {
             position: latLng
         });
 
-        bookstoresMarkers.push(marker);
+        bookstoreViewModel.markers.push(marker);
+        bookstoreViewModel.filteredMarkers.push(marker);
         google.maps.event.addListener(marker, 'click', function() {
             var self = this;
 
@@ -161,7 +123,6 @@ function bookstoresDetailsMarkers(map, label, frsqrBookItems) {
             } else {
                 self.setAnimation(google.maps.Animation.BOUNCE);
             }
-
 
             content = content + "</br>";
             content = content + "<p> " + this.title + "</p>";
@@ -174,45 +135,12 @@ function bookstoresDetailsMarkers(map, label, frsqrBookItems) {
     }
 }
 
-/*
-function bakeriesDetailsMarkers(map,label,frsqrBakeryItems) {
-    var content = "";
-    var marker;
-    for (var i = 0; i < frsqrBakeryItems.length; i++) {
-       var lat = frsqrBakeryItems[i].venue.lat;
-       var lng = frsqrBakeryItems[i].venue.lng;
-       var latLng = {lat: lat, lng: lng};
-       marker = new google.maps.Marker({
-         map: map,
-         label: label,
-         title: frsqrBakeryItems[i].venue.name,
-         id: i,
-         position: latLng
-       });
-       bakeriesMarkers.push(marker);
-    google.maps.event.addListener(marker, 'click', function() {
-        var self = this;
-            content = content + "</br>";
-            content = content + "<p> " + this.title + "</p>";
-            content = content + "<img src=\"" + bakeryPhotos[this.id][0].prefix + "100x100" + bakeryPhotos[this.id][0].suffix + "\"/>";
-            if(contentPhotoUrl != null){
-              content = content + "<img src=\"" + bakeryPhotos[i][0] + "\"/>";
-            }
-        infowindow.setContent(content);
-        infowindow.open(map, this);
-        contentPhotoUrl = null;
-        content = "";
-    });
-  }
-
-}
-*/
 function initialize() {
     // Create a map to show the results, and an info window to
     // pop up if the user clicks on the place marker.
 
     // Request for latitide and longitude of Fisherman's wharf, SanFrancisco, CA.
-    geocoder = new google.maps.Geocoder();
+    var geocoder = new google.maps.Geocoder();
     var address = 'fisherman\'s wharf, sanfrancisco, CA, USA';
     //getBookstores(sfo);
     geocoder.geocode({
@@ -221,9 +149,6 @@ function initialize() {
         if (status == google.maps.GeocoderStatus.OK) {
             geocoderSearchResult = results[0];
             map.setCenter(geocoderSearchResult.geometry.location);
-            //map.setCenter(results[0].geometry.location);
-            //getBakeries(results[0]);
-            //getBookstores(geocoderSearchResult);
             getBookstores(geocoderSearchResult).then(function(response) {
                 processFrsqrBooks(response);
             }, function(error) {
@@ -271,40 +196,6 @@ function getBookstores(geocoderSearchResult) {
 }
 
 
-/*
-function getBookstores(geocoderSearchResult) {
-    map.setCenter(geocoderSearchResult.geometry.location);
-    // Create a list and display all the results.
-    var cll = geocoderSearchResult.geometry.location.lat() + "," + geocoderSearchResult.geometry.location.lng();
-    var foursquareQuery = "https://api.foursquare.com/v2/venues/explore?client_id=F0XYIB113FEQQVQFFK5DGZ4V5PJBZA2DRNAXHFUW1G3UBE3N&client_secret=ZYY5PZ15D02DLZ0D3RGBADODPBC1KMKX4ZIQ4XNDNLUKBKEB&v=20140701&ll=" + cll + "&radius=1000&query=books&venuePhotos=1&limit=50";
-    //var foursquareQuery = "https://api.foursquare.com/v2/venues/search?client_id=F0XYIB113FEQQVQFFK5DGZ4V5PJBZA2DRNAXHFUW1G3UBE3N&client_secret=ZYY5PZ15D02DLZ0D3RGBADODPBC1KMKX4ZIQ4XNDNLUKBKEB&v=20140701&ll=" + cll + "&radius=2000&query=books&intent=browse&limit=50";
-    getBooksRequest = new XMLHttpRequest();
-    if (!getBooksRequest) {
-        alert('Giving up getBookstores:( Cannot create an XMLHTTP instance');
-        return false;
-    }
-    getBooksRequest.onreadystatechange = processFrsqrBooks;
-    getBooksRequest.open('GET', foursquareQuery);
-    getBooksRequest.send();
-}
-
-function getBakeries(geocoderSearchResult) {
-    map.setCenter(geocoderSearchResult.geometry.location);
-    // Create a list and display all the results.
-    var cll = geocoderSearchResult.geometry.location.lat() + "," + geocoderSearchResult.geometry.location.lng();
-    // write location and phone number info to file.
-    //var foursquareQuery = "https://api.foursquare.com/v2/venues/search?client_id=F0XYIB113FEQQVQFFK5DGZ4V5PJBZA2DRNAXHFUW1G3UBE3N&client_secret=ZYY5PZ15D02DLZ0D3RGBADODPBC1KMKX4ZIQ4XNDNLUKBKEB&v=20140701&ll=" + cll + "&radius=2000&query=bakery&intent=browse&limit=50";
-    var foursquareQuery = "https://api.foursquare.com/v2/venues/explore?client_id=F0XYIB113FEQQVQFFK5DGZ4V5PJBZA2DRNAXHFUW1G3UBE3N&client_secret=ZYY5PZ15D02DLZ0D3RGBADODPBC1KMKX4ZIQ4XNDNLUKBKEB&v=20140701&ll=" + cll + "&radius=1000&query=books&venuePhotos=1&limit=50";
-    getBakeriesRequest = new XMLHttpRequest();
-    if (!getBakeriesRequest) {
-        alert('Giving up getBakeries:( Cannot create an XMLHTTP instance');
-        return false;
-    }
-    getBakeriesRequest.onreadystatechange = processFrsqrBakeries;
-    getBakeriesRequest.open('GET', foursquareQuery);
-    getBakeriesRequest.send();
-}
-*/
 function BakeryViewModel() {
     var self = this;
     self.bakeries = ko.observableArray([]);
@@ -318,6 +209,8 @@ function BookstoreViewModel() {
     self.selectedBookstoreNames = ko.observableArray([]);
     self.searchText = ko.observable();
     self.filteredVenues = ko.observableArray([]);
+    self.filteredMarkers = ko.observableArray([]);
+    self.markers = ko.observableArray([]);
 
 };
 
@@ -342,22 +235,25 @@ function indexOfVenue(element, index, array) {
 }
 //searchText
 function getSearchText(textBoxElem) {
-
     var text = textBoxElem.value.toLowerCase();
     bookstoreViewModel.searchText(text);
     console.log(JSON.stringify(bookstoreViewModel.filteredVenues));
-    //ko.applyBindings(bookstoreViewModel.filteredVenues(), document.getElementById('bookstoresList'));
     if (text !== '') {
-        //bookstoreViewModel.bookstoreNames( bookstoreViewModel.bookstoreNames().map(function(item,index,array){
         bookstoreViewModel.filteredVenues(bookstoreViewModel.bookstoreNames().map(function(item, index, array) {
             if (array[index].toLowerCase().startsWith(text)) {
+                //bookstoreViewModel.filteredMarkers().push(bookstoreViewModel.markers()[index]);
                 return array[index];
             }
+            else{
+                bookstoreViewModel.markers()[index].setMap(null); 
+            }
+
         }));
     } else {
-
-        bookstoreViewModel.filteredVenues(bookstoreViewModel.bookstoreNames());
-
+       for(var m = 0; m < bookstoreViewModel.markers().length; m++){ 
+          bookstoreViewModel.markers()[m].setMap(map);
+       }
+          bookstoreViewModel.filteredVenues(bookstoreViewModel.bookstoreNames());
     }
     console.log(JSON.stringify(bookstoreViewModel.filteredVenues()));
 };
@@ -369,30 +265,14 @@ function displaySelection() {
     if (elem.selectedIndex == -1)
         return null;
     var numSelected = bookstoreViewModel.selectedBookstoreNames().length;
-  /*var lineSymbol = {
-    path: google.maps.SymbolPath.CIRCLE,
-    scale: 8,
-    strokeColor: '#393'
-  };
-  var line = new google.maps.Polyline({
-//http://stackoverflow.com/questions/14288358/google-maps-polyline-how-to-get-a-transparent-invisible-polyline 
-    icons: [{
-      icon: lineSymbol,
-      offset: '100%'
-    }],
-    strokeColor: '#000000',
-    strokeOpacity: 1.0, 
-    strokeWeight: 3,
-    map: map
-  });*/
 
     for (var i = 0; i < numSelected; i++) {
         var elem = bookstoreViewModel.selectedBookstoreNames()[i];
         // find the index in bookstores array corresponding to elem.
         var ind = bookstoreViewModel.bookstoreNames().findIndex(indexOfVenue, elem);
         console.log(ind);
-        if ((ind >= 0) && (ind < bookstoresMarkers.length)) {
-            var currentMarker = bookstoresMarkers[ind];
+        if ((ind >= 0) && (ind < bookstoreViewModel.markers().length)) {
+            var currentMarker =  bookstoreViewModel.markers()[ind];
             currentMarker.setAnimation(google.maps.Animation.DROP);
             if (currentMarker.getAnimation() !== null) {
                 currentMarker.setAnimation(null);
@@ -413,12 +293,12 @@ function displaySelection() {
 function animateCircle(line) {
     var count = 0;
     var intervalId = window.setInterval(function() {
-      count = (count + 1) % 200;
-      var icons = line.get('icons');
-      icons[0].offset = (count / 2) + '%';
-      line.set('icons', icons);
+        count = (count + 1) % 200;
+        var icons = line.get('icons');
+        icons[0].offset = (count / 2) + '%';
+        line.set('icons', icons);
     }, 20);
-    var timeoutId = window.setTimeout(stopAnimation,10000,intervalId);
+    var timeoutId = window.setTimeout(stopAnimation, 10000, intervalId);
 }
 
 function stopAnimation(intervalId) {
@@ -435,7 +315,6 @@ $('#checkboxParkingLots').on('change', function() {
             parkingLotsMarkers[m].setMap(null);
         }
     } else {
-        $('#parkingLotsList').show();
         for (var m = 0; m < parkingLotsMarkers.length; m++) {
             parkingLotsMarkers[m].setMap(map);
         }
@@ -459,13 +338,13 @@ $('#checkboxBookstores').on('change', function() {
     if (($(this).is(':checked'))) {
         // display markers for parking lots.
         $('#bookstoresList').hide();
-        for (var m = 0; m < bookstoresMarkers.length; m++) {
-            bookstoresMarkers[m].setMap(null);
+        for (var m = 0; m < bookstoreViewModel.markers().length; m++) {
+            bookstoreViewModel.markers()[m].setMap(null);
         }
     } else {
         $('#bookstoresList').show();
-        for (var m = 0; m < bookstoresMarkers.length; m++) {
-            bookstoresMarkers[m].setMap(map);
+        for (var m = 0; m < bookstoreViewModel.markers().length; m++) {
+            bookstoreViewModel.markers()[m].setMap(map);
         }
     }
 });
